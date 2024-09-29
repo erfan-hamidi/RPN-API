da90ffc06af637668e44ee1b2de8cd5f032457ad
erfan-hamidi <erfan9757@gmail.com>
2024-09-29 19:57:25 +0330

    add user model

--- cmd/models/user.go
+++ cmd/models/user.go
@@ -1,2 +1,28 @@
 package models
 
+import "database/sql"
+
+
+type User struct {
+    Username   string
+    Password   string
+    PrivateKey string
+}
+
+func FindUserByUsername(db *sql.DB, username string) (*User, error) {
+    var user User
+    row := db.QueryRow("SELECT * FROM users WHERE username=$1", username)
+    
+    err := row.Scan(&user.Username, &user.Password, &user.PrivateKey)
+    
+    if err != nil {
+        return nil, err
+    }
+
+    return &user, nil
+}
+
+func SaveUser(db *sql.DB, user *User) error {
+	_,err := db.Exec("INSERT INTO users (username, public_key, private_key) VALUES ($1, $2, $3)",user.Username,user.Password)
+	return err
+}
